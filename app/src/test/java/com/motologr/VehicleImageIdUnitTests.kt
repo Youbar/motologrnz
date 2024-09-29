07cc100f628f3a19d8bc4e2f78cfde8220760b61
Alfred E. Neuman <kent.thomas@hotmail.com>
2024-09-29 21:51:41 +1300

    Resolve unit test failure for VehicleImageIdUnitTests

--- app/src/test/java/com/motologr/VehicleImageIdUnitTests.kt
+++ app/src/test/java/com/motologr/VehicleImageIdUnitTests.kt
@@ -1,15 +1,26 @@
 package com.motologr
 
 import com.motologr.data.DataManager
+import com.motologr.data.objects.vehicle.Vehicle
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 class VehicleImageIdUnitTests : UnitTestBase() {
+    private lateinit var vehicle : Vehicle
+
+    @Before
+    fun setupTest() {
+        if (!this::vehicle.isInitialized) {
+            vehicle = returnDefaultVehicle()
+            DataManager.createNewVehicle(vehicle)
+            DataManager.setActiveVehicle(0)
+            vehicle = DataManager.returnActiveVehicle()!!
+        }
+    }
+
     @Test()
     fun updateVehicleImageId_NoOverflow_0() {
-        val vehicle = returnDefaultVehicle()
-        DataManager.createNewVehicle(vehicle)
-        DataManager.setActiveVehicle(0)
 
         val newVehicleImageId = DataManager.changeActiveVehicleImageId(false)
 
@@ -18,11 +29,7 @@ class VehicleImageIdUnitTests : UnitTestBase() {
 
     @Test()
     fun updateVehicleImageId_NoOverflow_1() {
-        val vehicle = returnDefaultVehicle()
-        DataManager.createNewVehicle(vehicle)
-        DataManager.setActiveVehicle(0)
-        DataManager.returnActiveVehicle()!!.vehicleImage = 1
-
+        vehicle.vehicleImage = 1
         val newVehicleImageId = DataManager.changeActiveVehicleImageId(false)
 
         assertEquals(2, newVehicleImageId)
@@ -30,11 +37,7 @@ class VehicleImageIdUnitTests : UnitTestBase() {
 
     @Test
     fun updateVehicleImageId_Overflow() {
-        val vehicle = returnDefaultVehicle()
         vehicle.vehicleImage = 2
-        DataManager.createNewVehicle(vehicle)
-        DataManager.setActiveVehicle(0)
-
         val newVehicleImageId = DataManager.changeActiveVehicleImageId(false)
 
         assertEquals(0, newVehicleImageId)
@@ -42,11 +45,7 @@ class VehicleImageIdUnitTests : UnitTestBase() {
 
     @Test
     fun updateVehicleImageId_NoOverflow_ArtPackEnabled() {
-        val vehicle = returnDefaultVehicle()
         vehicle.vehicleImage = 2
-        DataManager.createNewVehicle(vehicle)
-        DataManager.setActiveVehicle(0)
-
         val newVehicleImageId = DataManager.changeActiveVehicleImageId(true)
 
         assertEquals(3, newVehicleImageId)
@@ -54,11 +53,7 @@ class VehicleImageIdUnitTests : UnitTestBase() {
 
     @Test
     fun updateVehicleImageId_Overflow_ArtPackEnabled() {
-        val vehicle = returnDefaultVehicle()
         vehicle.vehicleImage = 7
-        DataManager.createNewVehicle(vehicle)
-        DataManager.setActiveVehicle(0)
-
         val newVehicleImageId = DataManager.changeActiveVehicleImageId(true)
 
         assertEquals(0, newVehicleImageId)
